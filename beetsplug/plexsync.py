7507b75ff4ad35ebbad92d86cdcb1981d4d4aeb5
Alok Saboo <arsaboo@gmx.com>
2023-03-07 08:02:04 -0500

    Add basic plexsync and playlist features

--- beetsplug/plexsync.py
+++ beetsplug/plexsync.py
@@ -7,33 +7,40 @@ Put something like the following in your config.yaml to configure:
         token: token
 """
 
-import os
+import time
 
-from beets import config, ui
-from beets.dbcore import types
-from beets.plugins import BeetsPlugin
 from plexapi import exceptions
 from plexapi.server import PlexServer
 
+from beets import config, ui
+from beets.dbcore import types
+from beets.dbcore.query import MatchQuery
+from beets.library import DateType
+from beets.plugins import BeetsPlugin
+
 
 class PlexSync(BeetsPlugin):
+    """Define plexsync class."""
     data_source = 'Plex'
 
     item_types = {
-        'plex_key': types.STRING,
         'plex_guid': types.STRING,
         'plex_ratingkey': types.INTEGER,
         'plex_userrating': types.FLOAT,
         'plex_skipcount': types.INTEGER,
         'plex_viewcount': types.INTEGER,
+        'plex_lastviewedat': DateType(),
+        'plex_lastratedat': DateType(),
+        'plex_updated': DateType(),
     }
 
     def __init__(self):
+        """Initialize plexsync plugin."""
         super().__init__()
 
         # Adding defaults.
         config['plex'].add({
-            'baseurl': 'localhost',
+            'host': 'localhost',
             'port': 32400,
             'token': '',
             'library_name': 'Music',
@@ -41,26 +48,29 @@ class PlexSync(BeetsPlugin):
             'ignore_cert_errors': False})
 
         config['plex']['token'].redact = True
+        baseurl = "http://" + config['plex']['host'].get() + ":" \
+            + str(config['plex']['port'].get())
         try:
-            self.plex = PlexServer(config['plex']['baseurl'].get(),
-                          config['plex']['token'].get())
+            self.plex = PlexServer(baseurl,
+                                   config['plex']['token'].get())
         except exceptions.Unauthorized:
             raise ui.UserError('Plex authorization failed')
         try:
             self.music = self.plex.library.section(
                 config['plex']['library_name'].get())
         except exceptions.NotFound:
-            raise ui.UserError(f"{config['plex']['library_name']} library not found")
+            raise ui.UserError(f"{config['plex']['library_name']} \
+                library not found")
         self.register_listener('database_change', self.listen_for_db_change)
 
     def listen_for_db_change(self, lib, model):
-        """Listens for beets db change and register the update for the end"""
+        """Listens for beets db change and register the update for the end."""
         self.register_listener('cli_exit', self._plexupdate)
 
     def commands(self):
+        """Add beet UI commands to interact with Plex."""
         plexupdate_cmd = ui.Subcommand(
-            'plexupdate', help=f'Update {self.data_source} library'
-        )
+            'plexupdate', help=f'Update {self.data_source} library')
 
         def func(lib, opts, args):
             self._plexupdate()
@@ -85,9 +95,9 @@ class PlexSync(BeetsPlugin):
 
         # plexplaylistadd command
         playlistadd_cmd = ui.Subcommand('plexplaylistadd',
-                                     help="add tracks to Plex playlist")
+                                        help="add tracks to Plex playlist")
 
-        playlistadd_cmd.parser.add_option('-p', '--playlist',
+        playlistadd_cmd.parser.add_option('-m', '--playlist',
                                           default='Beets',
                                           help='add playlist to Plex')
 
@@ -99,11 +109,11 @@ class PlexSync(BeetsPlugin):
 
         # plexplaylistremove command
         playlistrem_cmd = ui.Subcommand('plexplaylistremove',
-                                     help="add tracks to Plex playlist")
+                                        help="Plex playlist to edit")
 
-        playlistrem_cmd.parser.add_option('-p', '--playlist',
+        playlistrem_cmd.parser.add_option('-m', '--playlist',
                                           default='Beets',
-                                          help='add playlist to Plex')
+                                          help='Plex playlist to edit')
 
         def func_playlist_rem(lib, opts, args):
             items = lib.items(ui.decargs(args))
@@ -111,11 +121,22 @@ class PlexSync(BeetsPlugin):
 
         playlistrem_cmd.func = func_playlist_rem
 
-        return [plexupdate_cmd, sync_cmd, playlistadd_cmd, playlistrem_cmd]
+        # plexsyncrecent command - instead of using the plexsync command which
+        # can be slow, we can use the plexsyncrecent command to update info
+        # for tracks played in the last 7 days.
+        syncrecent_cmd = ui.Subcommand('plexsyncrecent',
+                                       help="Sync recently played tracks")
+
+        def func_sync_recent(lib, opts, args):
+            self._update_recently_played(lib)
+
+        syncrecent_cmd.func = func_sync_recent
+
+        return [plexupdate_cmd, sync_cmd, playlistadd_cmd, playlistrem_cmd,
+                syncrecent_cmd]
 
     def _plexupdate(self):
         """Update Plex music library."""
-
         try:
             self.music.update()
             self._log.info('Update started.')
@@ -125,7 +146,6 @@ class PlexSync(BeetsPlugin):
 
     def _fetch_plex_info(self, items, write, force):
         """Obtain track information from Plex."""
-
         for index, item in enumerate(items, start=1):
             self._log.info('Processing {}/{} tracks - {} ',
                            index, len(items), item)
@@ -140,12 +160,14 @@ class PlexSync(BeetsPlugin):
             if plex_track is None:
                 self._log.info('No track found for: {}', item)
                 continue
-            item.plex_key = plex_track.key
             item.plex_guid = plex_track.guid
             item.plex_ratingkey = plex_track.ratingKey
             item.plex_userrating = plex_track.userRating
             item.plex_skipcount = plex_track.skipCount
             item.plex_viewcount = plex_track.viewCount
+            item.plex_lastviewedat = plex_track.lastViewedAt
+            item.plex_lastratedat = plex_track.lastRatedAt
+            item.plex_updated = time.time()
             item.store()
             if write:
                 item.try_write()
@@ -155,61 +177,85 @@ class PlexSync(BeetsPlugin):
         tracks = self.music.searchTracks(
             **{'album.title': item.album, 'track.title': item.title})
         if len(tracks) == 1:
-            if self.compare_file_name(tracks[0], item):
-                return tracks[0]
-            #return tracks[0]
+            return tracks[0]
         elif len(tracks) > 1:
             for track in tracks:
-                if self.compare_file_name(track, item):
+                if track.parentTitle == item.album \
+                   and track.title == item.title:
                     return track
         else:
+            self._log.debug('Track {} not found in Plex library', item)
             return None
-        if len(tracks) == 0:
-            return None
-        elif len(tracks) == 1:
-            return tracks[0]
-
-    def get_plex_filename(self, track):
-        """Fetch Plex filename."""
-        return os.path.basename(track.media[0].parts[0].file)
-
-    def compare_file_name(self, track, item):
-        """Compare file names."""
-        if self.get_plex_filename(track) == os.path.basename(item.path):
-            return True
-        else:
-            return False
 
     def _plex_add_playlist_item(self, items, playlist):
         """Add items to Plex playlist."""
-        self._log.info('Adding {} tracks to {} playlist',
-                       len(items), playlist)
+        plex_set = set()
         try:
             plst = self.plex.playlist(playlist)
             playlist_set = set(plst.items())
         except exceptions.NotFound:
             plst = None
             playlist_set = set()
-        plex_set = {self.plex.fetchItem(item.plex_ratingkey)
-                    for item in items}
+        for item in items:
+            try:
+                plex_set.add(self.plex.fetchItem(item.plex_ratingkey))
+            except (exceptions.NotFound, AttributeError) as e:
+                self._log.warning('{} not found in Plex library. Error: {}',
+                                  item, e)
+                continue
         to_add = plex_set - playlist_set
+        self._log.info('Adding {} tracks to {} playlist',
+                       len(to_add), playlist)
         if plst is None:
             self._log.info('{} playlist will be created', playlist)
-            self.plex.createPlaylist(playlist, items = list(to_add))
+            self.plex.createPlaylist(playlist, items=list(to_add))
         else:
-            plst.addItems(items = list(to_add))
+            plst.addItems(items=list(to_add))
 
     def _plex_remove_playlist_item(self, items, playlist):
         """Remove items from Plex playlist."""
-        self._log.info('Removing {} tracks from {} playlist',
-                       len(items), playlist)
+        plex_set = set()
         try:
             plst = self.plex.playlist(playlist)
             playlist_set = set(plst.items())
         except exceptions.NotFound:
             self._log.error('{} playlist not found', playlist)
             return
-        plex_set = {self.plex.fetchItem(item.plex_ratingkey)
-                    for item in items}
+        for item in items:
+            try:
+                plex_set.add(self.plex.fetchItem(item.plex_ratingkey))
+            except (exceptions.NotFound) as e:
+                self._log.warning('{} not found in Plex library. Error: {}',
+                                  item, e)
+                continue
         to_remove = plex_set.intersection(playlist_set)
-        plst.removeItems(items = list(to_remove))
+        self._log.info('Removing {} tracks from {} playlist',
+                       len(to_remove), playlist)
+        plst.removeItems(items=list(to_remove))
+
+    def _update_recently_played(self, lib):
+        """Fetch the Plex track key."""
+        tracks = self.music.search(
+            filters={'track.lastViewedAt>>': '7d'}, libtype='track')
+        self._log.info("Updating information for {} tracks", len(tracks))
+        with lib.transaction():
+            for track in tracks:
+                query = MatchQuery("plex_ratingkey", track.ratingKey,
+                                   fast=False)
+                items = lib.items(query)
+                if not items:
+                    self._log.debug("{} | track not found", query)
+                    continue
+                elif len(items) == 1:
+                    self._log.info("Updating information for {} ", items[0])
+                    items[0].plex_userrating = track.userRating
+                    items[0].plex_skipcount = track.skipCount
+                    items[0].plex_viewcount = track.viewCount
+                    items[0].plex_lastviewedat = track.lastViewedAt
+                    items[0].plex_lastratedat = track.lastRatedAt
+                    items[0].plex_updated = time.time()
+                    items[0].store()
+                    items[0].try_write()
+                else:
+                    self._log.debug("Please sync Plex library again")
+                    continue
