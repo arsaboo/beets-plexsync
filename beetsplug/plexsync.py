fb27b01e38eeaf0794c1537e06446c1c0e73be0c
Alok Saboo <arsaboo@gmx.com>
2023-05-14 21:25:01 -0400

    Update plexsync.py

--- beetsplug/plexsync.py
+++ beetsplug/plexsync.py
@@ -18,6 +18,7 @@ import dateutil.parser
 import requests
 import spotipy
 from beets import config, ui
+from beets.ui import print
 from beets.dbcore import types
 from beets.dbcore.query import MatchQuery
 from beets.library import DateType
