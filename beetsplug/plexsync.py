916c9a7b49826968ec6ff4ebf004a7efd9a91fd3
Alok Saboo <arsaboo@gmx.com>
2023-07-20 09:53:55 -0400

    Update plexsync.py

--- beetsplug/plexsync.py
+++ beetsplug/plexsync.py
@@ -606,7 +606,7 @@ class PlexSync(BeetsPlugin):
                        len(to_add), playlist)
         if col is None:
             self._log.info('{} collection will be created', playlist)
-            self.plex.createCollection(playlist, items=list(to_add))
+            self.music.createCollection(playlist, items=list(to_add))
         else:
             try:
                 col.addItems(items=list(to_add))
