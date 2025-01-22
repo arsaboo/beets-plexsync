daa7ef252765c7c3a2b9923185d27309eabd57d7
Alok Saboo <arsaboo@gmx.com>
2025-01-22 16:58:55 -0500

    Updated user agent

--- beetsplug/plexsync.py
+++ beetsplug/plexsync.py
@@ -80,7 +80,13 @@ class PlexSync(BeetsPlugin):
         super().__init__()
 
         self.headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+            "Accept-Language": "en-US,en;q=0.9",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Connection": "keep-alive",
+            "Upgrade-Insecure-Requests": "1",
+            "DNT": "1",  # Do Not Track Request Header
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
         }
 
         # Add event loop initialization
